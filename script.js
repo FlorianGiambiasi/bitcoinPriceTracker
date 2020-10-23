da4d3e7c369a48e40bf9b27a0fe3ddfc8c797f32
Florian Giambiasi <fgmb@pm.me>
2020-10-23 10:31:01 +0200

    add json

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const url = 'https://blockchain.info/ticker';
 
+let requete = new XMLHttpRequest();
+requete.open('GET','url');
+
+requete.responseType = 'json';
+
+requete.send();
+
+requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+        if (requete.status === 200) {
+            let response = requete.response;
+            console.log(response);
+        }
+    }
+}
